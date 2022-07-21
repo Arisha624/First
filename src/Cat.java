86b6e0aae0ba09f2576447a0584a8b1f06521e91
Arisha624 <shibaevdima90@mail.ru>
2022-07-21 21:51:09 +0300

    Добавили собаку

---
+++ src/Cat.java
@@ -0,0 +1,20 @@
+public class Cat {
+    String name;
+    int age;
+    String color;
+
+    void voice(){
+        System.out.println("meow");
+    }
+
+    void eat(){
+        System.out.println("hrum");
+    }
+
+    int jump(){
+        System.out.println(name + " chponks");
+        return 6;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        Cat barsik = new Cat();
+        barsik.age = 10;
+        barsik.color = "Puppure";
+        barsik.name = "BarsikStringVat public static void main(String[] args) {";
+
+
+        barsik.jump();
+
+        Cat vasya = new Cat();
+        vasya.name = "Vasya";
+        vasya.jump();
+
+        int number = vasya.jump();
+        System.out.println("number = " + number);
+
+        System.out.println(6 + 11);
+        System.out.println(2 + "m" + 11);
     }
 }
